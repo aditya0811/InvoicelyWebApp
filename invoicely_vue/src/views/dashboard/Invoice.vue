1e4548695e5a857218d71a285321bd220952aad6
Aditya Mahajan <aditya.m.1997@gmail.com>
2023-06-07 22:34:41 +0530

    Part 9 Update invoice page

--- invoicely_vue/src/views/dashboard/Invoice.vue
+++ invoicely_vue/src/views/dashboard/Invoice.vue
@@ -13,39 +13,71 @@
             <div class="column is-12">
                 <h1 class="title">Invoice - {{ invoice.invoice_number }}</h1>
 
-                <hr>
+               
 
                 <button @click="getPdf()" class="button is-dark">Download PDF</button>
             </div>
 
-            <div class="column is-12">
-                <h3 class="is-size-4">Client</h3>
-                <p><strong>{{ invoice.client_name }}</strong></p>
-                <p v-if="invoice.client_address1">{{ invoice.client_address1 }}</p>
-                <p v-if="invoice.client_address2">{{ invoice.client_address2 }}</p>
-                <p v-if="invoice.client_zipcode || invoice.client_place">{{ invoice.client_place }} {{ invoice.client_zipcode }}</p>
-                <p v-if="invoice.client_country">{{ invoice.client_country }}</p>
+            <!-- mb-4 will provide gap below , and box argument will provide a shadow type box, helps in separating -->
+
+            <div class="column is-12 mb-4">
+                <div class="box">
+                    <h3 class="is-size-4 mb-5">Client</h3>
+                    <p><strong>{{ invoice.client_name }}</strong></p>
+                    <p v-if="invoice.client_address1">{{ invoice.client_address1 }}</p>
+                    <p v-if="invoice.client_address2">{{ invoice.client_address2 }}</p>
+                    <p v-if="invoice.client_zipcode || invoice.client_place">{{ invoice.client_place }} {{ invoice.client_zipcode }}</p>
+                    <p v-if="invoice.client_country">{{ invoice.client_country }}</p>
+                </div>
+            </div>
+
+            <div class="column is-12 mb-4 ">
+                <div class="box">
+                    <h3 class="is-size-4 mb-5">Items</h3>
+                    <table class="table is-fullwidth">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Quantity</th>
+                                <th>Vat rate</th>
+                                <th>Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="item in invoice.items" v-bind:key="item.id">
+                                <td>{{ item.title }}</td>
+                                <td>{{ item.quantity}}</td>
+                                <td>{{ item.vat_rate }}%</td>
+                                <td>{{ getItemTotal(item) }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
             <div class="column is-12">
-                <h3 class="is-size-4">Items</h3>
-                <table class="table is-fullwidth">
-                    <thead>
-                        <tr>
-                            <td>Title</td>
-                            <td>Quantity</td>
-                            <td>Amount</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="item in invoice.items" v-bind:key="item.id">
-                            <td>{{ item.title }}</td>
-                            <td>{{ item.quantity}}</td>
-                            <td>{{ item.net_amount }}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div class="box">
+                    <h3 class="is-size-4 mb-5">Summary</h3>
+
+                    <div class="columns">
+                        <div class="column is-6">
+                            <p><strong>Net amount</strong>: {{ invoice.net_amount }}</p>
+                            <p><strong>Vat amount</strong>: {{ invoice.vat_amount }}</p>
+                            <p><strong>Gross amount</strong>: {{ invoice.gross_amount }}</p>
+                            <p><strong>Bank account</strong>: {{ invoice.bankaccount }}</p>
+                        </div>
+                    
+                        <div class="column is-6">
+                            <p><strong>Our reference</strong>: {{ invoice.sender_reference }}</p>
+                            <p><strong>Client reference</strong>: {{ invoice.client_contact_reference }}</p>
+                            <p><strong>Due date</strong>: {{ invoice.get_due_date_formatted }}</p>
+                            <p><strong>Status</strong>: {{ getStatusLabel() }}</p>
+                            <p><strong>Invoice type</strong>: {{ getInvoiceType() }}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </div>
     </div>
 </template>
@@ -89,6 +121,27 @@ export default {
                     console.log(JSON.stringify(error))
                 })
 
+        },
+        getStatusLabel() {
+            if (this.invoice.is_paid) {
+                return 'Is paid'
+            } else {
+                return 'Is not paid'
+            }
+        },
+        getInvoiceType() {
+            if (this.invoice.invoice_type === 'credit_note') {
+                return 'Credit note'
+            } else {
+                return 'Invoice'
+            }
+        },
+        getItemTotal(item) {
+            const unit_price = item.unit_price
+            const quantity = item.quantity
+            const total = item.net_amount + (item.net_amount * (item.vat_rate / 100))
+
+            return parseFloat(total).toFixed(2)
         }
     }
 }
